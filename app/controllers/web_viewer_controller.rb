db45a1e94b2f3cc9c1aa33a48762b585562da2c9
Dan Mashuda <dmashuda@ycp.edu>
2015-08-07 18:37:27 -0400

    added redirect

--- app/controllers/web_viewer_controller.rb
+++ app/controllers/web_viewer_controller.rb
@@ -3,7 +3,7 @@ class WebViewerController < ApplicationController
   def index
     hostname = request.host
 
-    @business = Business.find_by_domain(hostname) or raise ActionController::RoutingError.new('Not Found')
+    @business = Business.find_by_domain(hostname) or redirect_to "/signin"
     @locations = @business.locations
     @service_lists = @business.service_lists.order(rank: :desc)
 
